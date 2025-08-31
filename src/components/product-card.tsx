fad3b58824e1eabe345bcdff60678b297735a447
kteken10 <dissangfrancis@yahoo.com>
2025-08-31 15:32:38 +0100

    update

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -25,18 +25,24 @@ export default function ProductCard({ name, time, rating, price, image, orderf }
         <Text> {rating}</Text>
         <Text className="text-neutral-400 " >({orderf} +)</Text>
       </View>
-      <View className="flex-row items-center justify-center mt-2 space-x-4">
-        {/* Prix */}
-        <Text className="font-bold text-lg">{price}</Text>
 
-        {/* Bouton Add to Cart avec icône */}
-        <TouchableOpacity className="bg-primary-500 flex-row items-center rounded-full px-4 h-10">
+      <View className="flex-row items-center justify-center mt-2 mx-4">
+
+
+
+        <Text className="font-bold text-xm">{price}</Text>
+
+
+        <TouchableOpacity className="bg-primary-500 flex-row items-center rounded-full px-4 h-10 mx-1">
           <Text className="text-white text-sm font-medium">Add to cart</Text>
+
           <View className="bg-white w-6 h-6 ml-2 rounded-full flex items-center justify-center">
             <ShoppingCart size={16} color={colors.primary[500]} variant="Bulk" />
           </View>
         </TouchableOpacity>
+
       </View>
+
 
 
     </View>
